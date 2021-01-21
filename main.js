c8a87fd61a2c3cb2fea5f3711a48bd9e97762498
Derek <derekcdlin@gmail.com>
2021-01-20 21:28:15 -0500

    added challenge problems

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-function pAequorFactory(num, array){
+const pAequorFactory = (num, array) =>{
   const obj = {
     _specimenNum: num,
     _dna: array,
@@ -35,29 +35,74 @@ function pAequorFactory(num, array){
     toString(){
       return `Specimen Number: ${this.specimenNum}\nDNA: ${this.dna}`;
     },
-    compareDNA(obj){
+    compareDNA(obj, bool=true){
       let count = 0;
       for(let i = 0; i < Math.min(this.dna.length, obj.dna.length); i++){
         if(this.dna[i] === obj.dna[i]){
           count++;
         }
       }
-      const ret_perc = (count/Math.min(this.dna.length, obj.dna.length) * 100).toFixed(2);
-      console.log(`specimen #1 and specimen #2 have ${ret_perc}% DNA in common`);
+      const ret_perc = Math.round((count/Math.min(this.dna.length, obj.dna.length) * 100)*100)/100;
+      if(bool){
+        console.log(`specimen #1 and specimen #2 have ${string_ret}% DNA in common`);
+      }
+      return ret_perc;
     },
     willLikelySurvive(){
       const numCorG = this.dna.filter(base => (base === 'C' || base === 'G')).length;
       const percCorG = (numCorG/this.dna.length) * 100;
       return (percCorG >= 60);
+    },
+    complementStrand(){
+      const complement = this.dna.map((base) => {
+        switch(base){
+          case 'A':
+            return 'T';
+          case 'T':
+            return 'A';
+          case 'C':
+            return 'G';
+          case 'G':
+            return 'C';
+        }
+      });
+      return complement;
     }
   };
-
   return obj;
-}
+};
 
+const twoMostSimilarDNA = (arr) => {
+  let largestSimilarity = 0;
+  let index1;
+  let index2;
+  for(let i = 0; i < arr.length-1 ; i++){
+    for(let j = 1; j < arr.length; j++){
+      if(i == j){
+        continue;
+      }
+      let currentSimilarity = arr[i].compareDNA(arr[j],false);
+      if(currentSimilarity > largestSimilarity){
+        //console.log(`i:${i}\tj:${j}\tsimilarity:${currentSimilarity}`);
+        largestSimilarity = currentSimilarity;
+        index1 = i;
+        index2 = j;
+      }
+    }
+  }
+  console.log(`The two most similar DNAs are from species #${index1} and #${index2} at ${largestSimilarity}%.`);
+  console.log(arr[index1].toString());
+  console.log(arr[index2].toString());
+};
+
+//sample of 30 random species
 const pAequorStudy = [];
 for(let i = 0; i < 30; i++){
   pAequorStudy.push(pAequorFactory(i, mockUpStrand()));
   console.log(pAequorStudy[i].toString());
 }
-console.log(pAequorStudy);
+
+//complement strand test of last specie in pAequorStudy
+console.log(pAequorStudy[29].complementStrand());
+//twoMostSimilarDNA function test
+twoMostSimilarDNA(pAequorStudy);
