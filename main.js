bc9ee11e4919f6614e3ff77dbfb066647c5e5866
Derek <derekcdlin@gmail.com>
2021-01-20 20:37:39 -0500

    complete base project

--- main.js
+++ main.js
@@ -13,10 +13,51 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+function pAequorFactory(num, array){
+  const obj = {
+    _specimenNum: num,
+    _dna: array,
+    get specimenNum(){
+      return this._specimenNum;
+    },
+    get dna(){
+      return this._dna;
+    },
+    mutate(){
+      const randBaseIndex = Math.floor(Math.random() * this.dna.length);
+      let diffBase;
+      do{
+        diffBase = returnRandBase();
+      }while(diffBase === this.dna[randBaseIndex]);
+      this.dna[randBaseIndex] = diffBase;
+      return this.dna;
+    },
+    toString(){
+      return `Specimen Number: ${this.specimenNum}\nDNA: ${this.dna}`;
+    },
+    compareDNA(obj){
+      let count = 0;
+      for(let i = 0; i < Math.min(this.dna.length, obj.dna.length); i++){
+        if(this.dna[i] === obj.dna[i]){
+          count++;
+        }
+      }
+      const ret_perc = (count/Math.min(this.dna.length, obj.dna.length) * 100).toFixed(2);
+      console.log(`specimen #1 and specimen #2 have ${ret_perc}% DNA in common`);
+    },
+    willLikelySurvive(){
+      const numCorG = this.dna.filter(base => (base === 'C' || base === 'G')).length;
+      const percCorG = (numCorG/this.dna.length) * 100;
+      return (percCorG >= 60);
+    }
+  };
 
+  return obj;
+}
 
-
-
-
-
-
+const pAequorStudy = [];
+for(let i = 0; i < 30; i++){
+  pAequorStudy.push(pAequorFactory(i, mockUpStrand()));
+  console.log(pAequorStudy[i].toString());
+}
+console.log(pAequorStudy);
